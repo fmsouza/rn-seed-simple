8d4102e316410a1cdd101ce6c5b8d58d869fdbeb
Fred Souza <fredericoamsouza@gmail.com>
2017-01-30 15:56:07 -0200

    Fixing Android back button behavior

--- src/store.js
+++ src/store.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { BackAndroid } from 'react-native';
 import { createStore, applyMiddleware } from 'redux';
 import { addNavigationHelpers } from 'react-navigation';
 import ReduxThunk from 'redux-thunk';
 import { autoRehydrate } from 'redux-persist';
 import { connect } from 'react-redux';
 import AppReducer from './reducers';
-import { AppNavigator } from './router';
+import { AppNavigator, INITIAL_ROUTE } from './router';
+
+class BackButtonNavigator extends React.Component {
+
+    componentWillMount() {
+        BackAndroid.addEventListener('hardwareBackPress', () => {
+            const { state, goBack } = this.props.navigation;
+            const { index, routes } = state;
+            if (routes[index].routeName !== INITIAL_ROUTE) {
+                goBack();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    render() {
+        return <AppNavigator {...this.props} />;
+    }
+}
 
 export const AppWithNavigationState =
     connect(state => ({ nav: state.nav }))(({ dispatch, nav }) => (
-        <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
+        <BackButtonNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
     ));
 
 export const Store = createStore(AppReducer, {}, autoRehydrate(), applyMiddleware(ReduxThunk));
